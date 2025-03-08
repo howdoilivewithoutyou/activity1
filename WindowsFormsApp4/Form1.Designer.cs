f0fa1be56d35d710d67aa0f93070e4080c174281
howdoilivewithoutyou <ravenscott197@gmail.com>
2025-03-08 23:16:13 +0800

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.display = new System.Windows.Forms.TextBox();
+            this.seven = new System.Windows.Forms.Button();
+            this.eigth = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.times = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.history = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // display
+            // 
+            this.display.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.display.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.display, "display");
+            this.display.Name = "display";
+            this.display.TextChanged += new System.EventHandler(this.display_TextChanged);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.seven, "seven");
+            this.seven.Name = "seven";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eigth
+            // 
+            this.eigth.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.eigth, "eigth");
+            this.eigth.Name = "eigth";
+            this.eigth.UseVisualStyleBackColor = false;
+            this.eigth.Click += new System.EventHandler(this.eigth_Click);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.nine, "nine");
+            this.nine.Name = "nine";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // divide
+            // 
+            this.divide.BackColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.divide, "divide");
+            this.divide.Name = "divide";
+            this.divide.UseVisualStyleBackColor = false;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.four, "four");
+            this.four.Name = "four";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.five, "five");
+            this.five.Name = "five";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.six, "six");
+            this.six.Name = "six";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // times
+            // 
+            this.times.BackColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.times, "times");
+            this.times.Name = "times";
+            this.times.UseVisualStyleBackColor = false;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.one, "one");
+            this.one.Name = "one";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.two, "two");
+            this.two.Name = "two";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.three, "three");
+            this.three.Name = "three";
+            this.three.UseVisualStyleBackColor = false;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // minus
+            // 
+            this.minus.BackColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.minus, "minus");
+            this.minus.Name = "minus";
+            this.minus.UseVisualStyleBackColor = false;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.zero, "zero");
+            this.zero.Name = "zero";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.delete, "delete");
+            this.delete.Name = "delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.Color.LightGray;
+            resources.ApplyResources(this.equals, "equals");
+            this.equals.Name = "equals";
+            this.equals.UseVisualStyleBackColor = false;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.add, "add");
+            this.add.Name = "add";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            resources.ApplyResources(this.contextMenuStrip2, "contextMenuStrip2");
+            // 
+            // history
+            // 
+            this.history.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.history.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.history, "history");
+            this.history.Name = "history";
+            this.history.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.Controls.Add(this.history);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eigth);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eigth;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox history;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder calculation = new StringBuilder();
+        private ArrayList numbers = new ArrayList();
+        private ArrayList operations = new ArrayList();
+        private double total = 0;
+        private bool isDeleted = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void seven_Click(object sender, EventArgs e)
+        {
+            calculation.Append("7");
+            display.Text = calculation.ToString();
+        }
+        private void eigth_Click(object sender, EventArgs e)
+        {
+            calculation.Append("8");
+            display.Text = calculation.ToString();
+        }
+        private void nine_Click(object sender, EventArgs e)
+        {
+            calculation.Append("9");
+            display.Text = calculation.ToString();
+        }
+        private void divide_Click(object sender, EventArgs e)
+        {
+            calculation.Append("/");
+            display.Text = calculation.ToString();
+        }
+        private void four_Click(object sender, EventArgs e)
+        {
+            calculation.Append("4");
+            display.Text = calculation.ToString();
+        }
+        private void five_Click(object sender, EventArgs e)
+        {
+            calculation.Append("5");
+            display.Text = calculation.ToString();
+        }
+        private void six_Click(object sender, EventArgs e)
+        {
+            calculation.Append("6");
+            display.Text = calculation.ToString();
+        }
+        private void times_Click(object sender, EventArgs e)
+        {
+            calculation.Append("*");
+            display.Text = calculation.ToString();
+        }
+        private void one_Click(object sender, EventArgs e)
+        {
+            calculation.Append("1");
+            display.Text = calculation.ToString();
+        }
+        private void two_Click(object sender, EventArgs e)
+        {
+            calculation.Append("2");
+            display.Text = calculation.ToString();
+        }
+        private void three_Click(object sender, EventArgs e)
+        {
+            calculation.Append("3");
+            display.Text = calculation.ToString();
+        }
+        private void minus_Click(object sender, EventArgs e)
+        {
+            calculation.Append("-");
+            display.Text = calculation.ToString();
+        }
+        private void zero_Click(object sender, EventArgs e)
+        {
+            calculation.Append("0");
+            display.Text = calculation.ToString();
+        }
+        private void delete_Click(object sender, EventArgs e)
+        {
+            calculation.Clear();
+            total = 0;
+            numbers.Clear();
+            operations.Clear();
+            display.Text = "0";
+            history.Text = "";
+        }
+        private void equals_Click(object sender, EventArgs e)
+        {
+            if(CheckIfTheLastIsNotDigit())
+                MessageBox.Show("Math Error!");
+            else if(CheckIfThereAreThreeConsecutiveOperationsOrMore())
+                MessageBox.Show("Math Error!");
+            else
+                AddTheNumbersOperations();
+
+            ExecuteTheCalculation();
+            if(!isDeleted)
+            {
+                calculation.Clear();
+                calculation.Append(total);
+                numbers.Clear();
+                operations.Clear();
+            }
+           
+        }
+
+        public bool CheckIfTheLastIsNotDigit()
+        {
+            char a = calculation[calculation.Length - 1];         
+            return !(char.IsDigit(a));  
+        }
+        public bool CheckIfThereAreThreeConsecutiveOperationsOrMore()
+        {
+            for (int i = 0; i < calculation.Length - 3; ++i)
+            {
+                char g = calculation[i];
+                char l = calculation[i + 1];
+                char y = calculation[i + 2];
+                if (!char.IsDigit(g) && !char.IsDigit(l) && !char.IsDigit(y))               
+                    return true;
+            }
+            return false;
+        }
+        public void AddTheNumbersOperations()
+        {
+            StringBuilder sb = new StringBuilder();
+            string ngek = "";
+            bool isFound = false;
+
+            for (int i = 0; i < calculation.Length; ++i)
+            {
+                char c = calculation.ToString()[i];
+
+
+                if (i == 0 && (c == '+' || c == '/' || c == '*'))
+                {
+                    MessageBox.Show("Syntax Error!");
+                    return;
+                }
+                else if (i == 0 && c == '-')
+                {
+                    ngek = ngek + "-";
+                }
+                else if (char.IsDigit(c))
+                {
+                    ngek += c;
+
+                }
+                else if ((c == '+' || c == '-' || c == '/' || c == '*') && char.IsDigit(calculation[i + 1]))
+                {
+                    NextToDigit(ref ngek, c, isFound);
+
+                }
+                else if ((c == '+' || c == '-' || c == '/' || c == '*') && calculation[i + 1] == '-')
+                {
+                    NextToDigit(ref ngek, c, true);
+                    ++i;
+                }
+                if (i == calculation.Length - 1)
+                {
+                    try
+                    {
+                        numbers.Add(ngek);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("tryht");
+                    }
+                }
+            }
+
+        }
+        public void NextToDigit(ref string ngek, char c, bool isFound)
+        {
+            if(!isFound)
+            {
+                try
+                {
+                    numbers.Add(ngek);
+                    ngek = "";
+                    operations.Add(c);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("iNVALID");
+                }
+            }else
+            {
+                try
+                {
+                    numbers.Add(ngek);
+                    ngek = "-";
+                    operations.Add(c);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("iNVALID");
+                }
+            }
+            
+        }
+        public void ExecuteTheCalculation()
+        {
+            int num = 1;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == 0)
+                {
+                    char c = Convert.ToChar(operations[0]);
+
+                    switch (c)
+                    {
+                        case '/':
+                            total = Convert.ToDouble((numbers[i])) / Convert.ToDouble(numbers[i + 1]);
+                            break;
+                        case '*':
+                            total = Convert.ToInt16((numbers[i])) * Convert.ToInt16(numbers[i + 1]);
+                            break;
+                        case '-':
+                            total = Convert.ToInt16((numbers[i])) - Convert.ToInt16(numbers[i + 1]);
+                            break;
+                        case '+':
+                            total = Convert.ToInt16((numbers[i])) + Convert.ToInt16(numbers[i + 1]);
+                            break;
+                    }
+                } else if (i != 1)
+                {
+                    if (num < operations.Count)
+                    {
+                        char c = Convert.ToChar(operations[num]);
+                        switch (c)
+                        {
+                            case '/':
+                                total /= Convert.ToDouble((numbers[i]));
+                                break;
+                            case '*':
+                                total *= Convert.ToInt16((numbers[i]));
+                                break;
+                            case '-':
+                                total -= Convert.ToInt16((numbers[i]));
+                                break;
+                            case '+':
+                                total += Convert.ToInt16((numbers[i]));
+                                break;
+
+                        }
+                    }
+                    ++num;
+                }
+            }
+            string totalDecimal = $"{total:F2}";
+            display.Text = totalDecimal;
+            history.Text = calculation.ToString();
+
+        }
+        private void add_Click(object sender, EventArgs e)
+        {
+            calculation.Append("+");
+            display.Text = calculation.ToString();
+        }
+        private void display_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
